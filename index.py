2f6c5269800de89386bffe70e986f6ca0dae6d4b
Mohanish <111619160+Mohanish7777777@users.noreply.github.com>
2023-05-28 16:36:45 +0530

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryH
 from movies_scraper import search_movies, get_movie
 
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("6071733539:AAFK-lhLAZBurUeKkdTOn5uCBXpi5wGMSfQ")
 URL = "https://movie-bot-vert.vercel.app"
 bot = Bot(TOKEN)
 
